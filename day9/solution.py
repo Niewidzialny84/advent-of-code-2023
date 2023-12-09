13e62c34244ea3d9ef995e12668245d3b5365890
Niewidzialny84 <niewidzialny84@vp.pl>
2023-12-09 13:41:12 +0100

    Day9: Part2

--- day9/solution.py
+++ day9/solution.py
@@ -25,20 +25,49 @@ def differences(numbers: list) -> list:
         recur = differences(array)
         return recur + [array[len(array) - 1] + recur[len(recur) - 1]]
 
+def differencesBackwards(numbers: list) -> list:
+    array = []
 
-def processLine(line: str) -> list:
+    for i, number in enumerate(numbers):
+        if i >= len(numbers) - 1:
+            break
+        array.append((number - numbers[i + 1]) * -1)
     
+    zeroes = array.count(0)
+   
+    if array == []:
+        return [0]
+
+    if zeroes == len(array):
+        return [array[0]]
+    else:
+        recur = differencesBackwards(array)
+        return [array[0] - recur[0]] + recur
+
+def getNumbers(line: str) -> list:
     coreNumbers = []
     for number in line.split():
         if number == '' or number == '\n' or number == ' ':
             continue
 
         coreNumbers.append(int(number))
+    return coreNumbers
+  
+def processLineBackwards(line: str) -> list:
+    coreNumbers = getNumbers(line)
+    
+    arr = differencesBackwards(coreNumbers)
+    first = arr[0]
+    coreLast = coreNumbers[0]
+    arr = [coreLast - first] + arr
+    
+    return arr
+
+def processLine(line: str) -> list:
+    coreNumbers = getNumbers(line)
 
     arr = differences(coreNumbers)
     last = arr[len(arr) - 1]
-    if len(coreNumbers) == 0:
-        print(line)
     coreLast = coreNumbers[-1]
     arr.append(last + coreLast)
     
@@ -53,10 +82,13 @@ def sumLine(line: str) -> int:
 
     return result
 
-def sumLines(lines: list) -> int:
+def sumLines(lines: list, backwards: bool = True) -> int:
     result = 0
     for line in lines:
-        result += processLine(line)[-1]
+        if backwards:
+            result += processLineBackwards(line)[0]
+        else:
+            result += processLine(line)[-1]
 
     return result
 
